ac894f64d14944ebc2081215e371cc8baf4429d2
MuhammadSaeedAli123 <m.saeedali.28@gmail.com>
2024-05-05 01:59:22 +0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import CollaborativeEditor from './CollaborativeEditor';
 import reportWebVitals from './reportWebVitals';
+import MainPage from './MainPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <CollaborativeEditor />
   </React.StrictMode>
 );
 
